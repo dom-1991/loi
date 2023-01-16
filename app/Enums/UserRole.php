37a063f5c2e96d7f7e706eaaf343ed0a9798acbd
Dom <dom-1991@gmail.com>
2023-01-16 20:47:38 +0700

    feat: add user

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -6,9 +6,11 @@ use BenSampo\Enum\Enum;
 
 final class UserRole extends Enum
 {
-    const EMPLOYEE =   1;
-    const ADMIN =   2;
+    const EMPLOYEE = 1;
+    const ADMIN = 2;
     const SUPER_ADMIN = 3;
+    const POLICE = 4;
+    const RENT_BUILDING = 5;
 
     public static function getLabel()
     {
@@ -16,6 +18,8 @@ final class UserRole extends Enum
             self::EMPLOYEE => 'Nhân viên',
             self::ADMIN => 'Quản lý',
             self::SUPER_ADMIN => 'Siêu quản lý',
+            self::POLICE => 'Công An',
+            self::RENT_BUILDING => 'Thuê quán',
         ];
     }
 }
