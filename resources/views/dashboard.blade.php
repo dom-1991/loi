3efbe62086db0ea67784aa878e09f7aa8d051206
tabu <thangbh1@vmodev.com>
2023-01-16 17:32:38 +0700

    feat: update color

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <div class="container mt-3 homepage">
         <div class="row">
             <div class="col-12 mb-3">
-                <div class="rounded shadow {{ $price < 0 ? 'bg-danger' : 'bg-success' }} p-3">
+                <div class="rounded shadow {{ $price < 0 ? 'bg-danger-50' : 'bg-success-50' }} p-3">
                     <p>Lợi nhuận đã bao gồm lương:</p>
                     <p>
                         {{ number_format($price) }} đồng
