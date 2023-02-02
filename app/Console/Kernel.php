3f77754947a757ef63eb5bfb52862b98320d2af8
tabu <thangbh1@vmodev.com>
2023-02-02 17:18:02 +0700

    feat: update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('report:telegram')->dailyAt('15:26');
+         $schedule->command('report:telegram')->everyFiveMinutes();
     }
 
     /**
